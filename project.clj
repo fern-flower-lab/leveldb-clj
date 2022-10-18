ab57991cd20176d1491cea3cf59c64db8ab91937
MelKori <mieldo@gmail.com>
2022-10-18 22:05:44 +0300

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ai.z7/leveldb-clj "0.0.4"
+(defproject ai.z7/leveldb-clj "0.0.5"
   :description "The LevelDB driver in Clojure"
   :url "https://github.com/ai.z7/leveldb-clj"
   :license {:name "Built In Project License"}
